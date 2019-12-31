03d8219299d2d89bee913107e40114901c2a0de0
Wan Chun Chen <chen4119@gmail.com>
2019-12-30 19:10:24 -0500

    published 0.0.21

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,5 @@ export {render} from "./operators/render";
 export {aggregateBy} from "./operators/aggregateBy";
 export {paginate} from "./operators/paginate";
 export {toHtml} from "./operators/toHtml";
+export {pushSchemaOrgJsonLd} from "./operators/pushSchemaOrgJsonLd";
 export {OUTPUT_FOLDER, CACHE_FOLDER} from "./constants";
