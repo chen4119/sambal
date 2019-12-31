3186069b446d3082920d0375f7043b3dc7d06958
Wan Chun Chen <chen4119@gmail.com>
2019-12-31 17:13:26 -0500

    published 0.0.23

--- src/operators/toHtml.ts
+++ src/operators/toHtml.ts
@@ -1,13 +1,14 @@
 import {pipe} from "rxjs";
 import {map} from "rxjs/operators";
 import {addJsonLdToDOM} from "./addJsonLdToDOM";
+import {SAMBAL_INTERNAL} from "../constants";
 
 export function toHtml() {
     return pipe(
         addJsonLdToDOM(),
         map(d => {
-            if (d.html) {
-                const $ = d.html;
+            if (d[SAMBAL_INTERNAL] && d[SAMBAL_INTERNAL].html) {
+                const $ = d[SAMBAL_INTERNAL].html;
                 return $.html();
             }
             return "";
